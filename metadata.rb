6f05fe8b78c791aa8675f73403065e1f4372c886
Rafe Colton <r.colton@modcloth.com>
2013-03-01 17:58:29 -0500

    Adding cookbook name
    Signed-off-by: Jesse Szwedko <j.szwedko@modcloth.com>

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,7 @@
 maintainer       'ModCloth, Inc.'
 maintainer_email 'external.tools@modcloth.com'
+name             'golang'
 license          'All rights reserved'
 description      'Installs/Configures golang'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
-
-
