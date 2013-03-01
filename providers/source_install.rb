134624c536ba723fbd5250444b52136ae0b17c72
Rafe Colton <r.colton@modcloth.com>
2013-03-01 17:59:29 -0500

    First attempt at golang installer provider
    Signed-off-by: Dan Buch <d.buch@modcloth.com>

---
+++ providers/source_install.rb
@@ -0,0 +1,87 @@
+action :create do
+  manage_deps(:action => :install)
+
+  directory install_path do
+    owner 'root'
+    group 'root'
+    mode 0755
+    recursive true
+
+    action :create
+  end
+
+  remote_file tarball do
+    source "#{node['golang']['url']}#{version}.src.tar.gz"
+    checksum node['golang']['checksum']
+    mode 0644
+  end
+
+  bash 'build-and-install-golang' do
+    cwd Chef::Config[:file_cache_path]
+    code <<-EOF
+      tar -zxvf #{tarball}
+      mv go/* #{install_path}
+      (cd #{install_path}/src && ./all.bash)
+      (cd #{install_path}/bin && ln -s * #{node['install_prefix']}/bin)
+    EOF
+
+    not_if do
+      ::File.exists?("#{file_home}/go") &&
+        ::File.symlink?("#{file_home}/go") &&
+        ::File.mtime("#{file_home}/go") > ::File.mtime(tarball)
+    end
+  end
+
+  new_resource.updated_by_last_action(true)
+end
+
+action :delete do
+  manage_deps(:action => :uninstall)
+
+  Dir["#{install_path}/bin"].each do |f|
+    file "#{file_home}/#{f}" do
+      action :delete
+    end
+  end
+
+  directory install_path do
+    recursive true
+    action :delete
+  end
+
+  file tarball do
+    action :delete
+  end
+
+  new_resource.updated_by_last_action(true)
+end
+
+def manage_deps(opts)
+  deps.each do |dev_pkg|
+    package dev_pkg do
+      action opts[:action]
+    end
+  end
+end
+
+def deps
+  value_for_platform_family(
+    'default' => ['gcc', 'libc6-dev']
+  )
+end
+
+def version
+  @version ||= new_resource.name
+end
+
+def tarball
+  @tarball ||= "#{Chef::Config[:file_cache_path]}/golang-#{version}.tar.gz"
+end
+
+def install_path
+  @install_path ||= "#{node['install_prefix']}/share/go/#{version}"
+end
+
+def file_home
+  @file_home ||= "#{node['install_prefix']}/bin"
+end
